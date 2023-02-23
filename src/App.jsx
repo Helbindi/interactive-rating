f8a2b5f47efb0fe5274517bb125dfe53dd98c9bf
Andy <nunuzuhh@gmail.com>
2023-02-22 21:30:36 -0800

    work done?

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import Card from "./components/Card";
+import Form from "./components/Form";
+import Confirmation from "./components/Confirmation";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [rating, setRating] = useState();
+  const [submitted, setSubmitted] = useState(false);
+  const scores = [1, 2, 3, 4, 5];
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="rating-container">
+      {!submitted && (
+        <Card>
+          <Form
+            rating={rating}
+            setRating={setRating}
+            setSubmitted={setSubmitted}
+            scores={scores}
+          />
+        </Card>
+      )}
+
+      {submitted && (
+        <Card>
+          <Confirmation rating={rating} scores={scores} />
+        </Card>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
